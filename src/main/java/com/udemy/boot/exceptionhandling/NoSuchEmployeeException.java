2fdd1fda3dfcadc966ab617491f5beeb9c5d3b0a
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-10-03 18:23:59 +0300

    change service and controller layer by adding new spring data rest dependency

--- src/main/java/com/udemy/boot/exceptionhandling/NoSuchEmployeeException.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.boot.exceptionhandling;
-
-public class NoSuchEmployeeException extends RuntimeException {
-    public NoSuchEmployeeException(String message) {
-        super(message);
-    }
-}
